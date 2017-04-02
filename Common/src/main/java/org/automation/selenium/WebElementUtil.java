f040e63de3654c60c666fddcbc7ee30c53833184
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-04-01 23:50:35 -0400

    WebElementUtil java

---
+++ Common/src/main/java/org/automation/selenium/WebElementUtil.java
@@ -0,0 +1,22 @@
+package org.automation.selenium;
+
+import org.openqa.selenium.WebElement;
+
+
+public class WebElementUtil {
+
+    public static void print(WebElement element) {
+        System.out.println("TAG " + element.getTagName());
+        System.out.println("TXT " + element.getText());
+        System.out.println("VAL " + element.getAttribute("value"));
+        System.out.println(element.toString());
+    }
+
+    public static boolean isEnable(WebElement element){
+        return element.isEnabled();
+    }
+    public static String getValue(WebElement element){
+        return element.getAttribute("value");
+    }
+
+}
