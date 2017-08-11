5e392ede41aaa96a6340debbe8c7806708e00b64
sarkershantonu <shantonu_oxford@yahoo.com>
2017-08-10 23:59:51 -0400

    demo test works on linux

--- TestExamples/src/test/java/videorecording/TestVideoRecording.java
+++ TestExamples/src/test/java/videorecording/TestVideoRecording.java
@@ -1,7 +1,31 @@
 package videorecording;
 
+import com.automation.remarks.junit.VideoRule;
+import com.automation.remarks.video.annotations.Video;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by shantonu on 8/10/17.
  */
 public class TestVideoRecording {
+
+    @Rule
+    public VideoRule videoRule = new VideoRule();
+
+    @Test
+    @Video
+    public void shouldFailAndCreateRecordWithTestName() throws InterruptedException {
+        Thread.sleep(5000);
+        assert false;
+    }
+
+    @Test
+    @Video(name = "second_test")
+    public void videoShouldHaveNameSecondTest() throws InterruptedException {
+        Thread.sleep(10000);
+        assertTrue(false);
+    }
 }
